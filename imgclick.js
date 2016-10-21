41246b1f353ec93e9297d31bbd0bd0b8290c50d4
martinostrat <martin.ostrat@khk.ee>
2016-10-22 01:55:08 +0300

    ver 1.0

---
+++ imgclick.js
@@ -0,0 +1,297 @@
+//      1#
+$(function(){
+    $("#big-image img:eq(0)").nextAll().hide();
+    $(".small-images img").click(function(e){
+        var index = $(this).index();
+        $("#big-image img").eq(index).show().siblings().hide();
+    });
+});
+//      2#
+$(function(){
+    $("#big-image2 img:eq(0)").nextAll().hide();
+    $(".small-images2 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image2 img").eq(index).show().siblings().hide();
+    });
+});
+//      3#
+$(function(){
+    $("#big-image3 img:eq(0)").nextAll().hide();
+    $(".small-images3 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image3 img").eq(index).show().siblings().hide();
+    });
+});
+//      4#
+$(function(){
+    $("#big-image4 img:eq(0)").nextAll().hide();
+    $(".small-images4 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image4 img").eq(index).show().siblings().hide();
+    });
+});
+//      5#
+$(function(){
+    $("#big-image5 img:eq(0)").nextAll().hide();
+    $(".small-images5 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image5 img").eq(index).show().siblings().hide();
+    });
+});
+//      6#
+$(function(){
+    $("#big-image6 img:eq(0)").nextAll().hide();
+    $(".small-images6 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image6 img").eq(index).show().siblings().hide();
+    });
+});
+//      7#
+$(function(){
+    $("#big-image7 img:eq(0)").nextAll().hide();
+    $(".small-images7 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image7 img").eq(index).show().siblings().hide();
+    });
+});
+//      8#
+$(function(){
+    $("#big-image8 img:eq(0)").nextAll().hide();
+    $(".small-images8 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image8 img").eq(index).show().siblings().hide();
+    });
+});
+//      9#
+$(function(){
+    $("#big-image9 img:eq(0)").nextAll().hide();
+    $(".small-images9 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image9 img").eq(index).show().siblings().hide();
+    });
+});
+//      10#
+$(function(){
+    $("#big-image10 img:eq(0)").nextAll().hide();
+    $(".small-images10 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image10 img").eq(index).show().siblings().hide();
+    });
+});
+//      11#
+$(function(){
+    $("#big-image11 img:eq(0)").nextAll().hide();
+    $(".small-images11 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image11 img").eq(index).show().siblings().hide();
+    });
+});
+//      12#
+$(function(){
+    $("#big-image12 img:eq(0)").nextAll().hide();
+    $(".small-images12 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image12 img").eq(index).show().siblings().hide();
+    });
+});
+//      13#
+$(function(){
+    $("#big-image13 img:eq(0)").nextAll().hide();
+    $(".small-images13 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image13 img").eq(index).show().siblings().hide();
+    });
+});
+//      14#
+$(function(){
+    $("#big-image14 img:eq(0)").nextAll().hide();
+    $(".small-images14 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image14 img").eq(index).show().siblings().hide();
+    });
+});
+//      15#
+$(function(){
+    $("#big-image15 img:eq(0)").nextAll().hide();
+    $(".small-images15 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image15 img").eq(index).show().siblings().hide();
+    });
+});
+//      16#
+$(function(){
+    $("#big-image16 img:eq(0)").nextAll().hide();
+    $(".small-images16 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image16 img").eq(index).show().siblings().hide();
+    });
+});
+//      17#
+$(function(){
+    $("#big-image17 img:eq(0)").nextAll().hide();
+    $(".small-images17 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image17 img").eq(index).show().siblings().hide();
+    });
+});
+//      18#
+$(function(){
+    $("#big-image18 img:eq(0)").nextAll().hide();
+    $(".small-images18 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image18 img").eq(index).show().siblings().hide();
+    });
+});
+//      19#
+$(function(){
+    $("#big-image19 img:eq(0)").nextAll().hide();
+    $(".small-images19 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image19 img").eq(index).show().siblings().hide();
+    });
+});
+//      20#
+$(function(){
+    $("#big-image20 img:eq(0)").nextAll().hide();
+    $(".small-images20 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image20 img").eq(index).show().siblings().hide();
+    });
+});
+//      21#
+$(function(){
+    $("#big-image21 img:eq(0)").nextAll().hide();
+    $(".small-images21 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image21 img").eq(index).show().siblings().hide();
+    });
+});
+//      22#
+$(function(){
+    $("#big-image22 img:eq(0)").nextAll().hide();
+    $(".small-images22 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image22 img").eq(index).show().siblings().hide();
+    });
+});
+//      23#
+$(function(){
+    $("#big-image23 img:eq(0)").nextAll().hide();
+    $(".small-images23 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image23 img").eq(index).show().siblings().hide();
+    });
+});
+//      24#
+$(function(){
+    $("#big-image24 img:eq(0)").nextAll().hide();
+    $(".small-images24 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image24 img").eq(index).show().siblings().hide();
+    });
+});
+//      25#
+$(function(){
+    $("#big-image25 img:eq(0)").nextAll().hide();
+    $(".small-images25 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image25 img").eq(index).show().siblings().hide();
+    });
+});
+//      26#
+$(function(){
+    $("#big-image26 img:eq(0)").nextAll().hide();
+    $(".small-images26 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image26 img").eq(index).show().siblings().hide();
+    });
+});
+//      27#
+$(function(){
+    $("#big-image27 img:eq(0)").nextAll().hide();
+    $(".small-images27 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image27 img").eq(index).show().siblings().hide();
+    });
+});
+//      28#
+$(function(){
+    $("#big-image28 img:eq(0)").nextAll().hide();
+    $(".small-images28 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image28 img").eq(index).show().siblings().hide();
+    });
+});
+//      29#
+$(function(){
+    $("#big-image29 img:eq(0)").nextAll().hide();
+    $(".small-images29 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image29 img").eq(index).show().siblings().hide();
+    });
+});
+//      30#
+$(function(){
+    $("#big-image30 img:eq(0)").nextAll().hide();
+    $(".small-images30 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image30 img").eq(index).show().siblings().hide();
+    });
+});
+//      31#
+$(function(){
+    $("#big-image31 img:eq(0)").nextAll().hide();
+    $(".small-images31 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image31 img").eq(index).show().siblings().hide();
+    });
+});
+//      32#
+$(function(){
+    $("#big-image32 img:eq(0)").nextAll().hide();
+    $(".small-images32 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image32 img").eq(index).show().siblings().hide();
+    });
+});
+//      33#
+$(function(){
+    $("#big-image33 img:eq(0)").nextAll().hide();
+    $(".small-images33 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image33 img").eq(index).show().siblings().hide();
+    });
+});
+//      34#
+$(function(){
+    $("#big-image34 img:eq(0)").nextAll().hide();
+    $(".small-images34 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image34 img").eq(index).show().siblings().hide();
+    });
+});
+//      35#
+$(function(){
+    $("#big-image35 img:eq(0)").nextAll().hide();
+    $(".small-images35 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image35 img").eq(index).show().siblings().hide();
+    });
+});
+//      36#
+$(function(){
+    $("#big-image36 img:eq(0)").nextAll().hide();
+    $(".small-images36 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image36 img").eq(index).show().siblings().hide();
+    });
+});
+//      37#
+$(function(){
+    $("#big-image37 img:eq(0)").nextAll().hide();
+    $(".small-images37 img").click(function(e){
+        var index = $(this).index();
+        $("#big-image37 img").eq(index).show().siblings().hide();
+    });
+});
+//      38#
